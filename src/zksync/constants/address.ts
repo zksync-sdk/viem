73f428bd37ebc2a8920304fe1d6bd18240d503f8
danijelTxFusion <danijel@txfusion.io>
2025-04-08 00:00:01 +0200

    fix: adjust withdraw to work with protocol version v26

--- src/zksync/constants/address.ts
+++ src/zksync/constants/address.ts
@@ -21,4 +21,10 @@ export const l1MessengerAddress =
 export const l1ToL2AliasOffset =
   '0x1111000000000000000000000000000000001111' as const
 
+export const l2AssetRouterAddress =
+  '0x0000000000000000000000000000000000010003' as const
+
+export const l2NativeTokenVaultAddress =
+  '0x0000000000000000000000000000000000010004' as const
+
 export const addressModulo = 2n ** 160n
